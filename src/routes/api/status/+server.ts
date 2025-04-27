2ee6014f169a1c3b5a51c3c9def4432ac662add9
ohager <oliver@devbutze.com>
2025-04-27 16:14:12 -0300

    chore: set version identifier

--- src/routes/api/status/+server.ts
+++ src/routes/api/status/+server.ts
@@ -1,4 +1,4 @@
 import { messageResponse } from '$lib/server/messageResponse';
 export function GET() {
-	return new Response(messageResponse('Running fine...'));
+	return new Response(messageResponse('[V2] - Running fine...'));
 }
